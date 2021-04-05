d5a5ec50e2905924c78eb58f8358d93facf3b5f1
Syed Nazmus Shakib <sakibsyed001@outlook.com>
2021-04-05 17:33:30 +0600

    route

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { createStackNavigator } from "@react-navigation/stack"
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import WelcomeScreen from "../screens/WelcomeScreen"
-import LoginScreen from "../screens/LoginScreen"
-import RegisterScreen from "../screens/RegisterScreen"
+import WelcomeScreen from "../screens/WelcomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const AuthNavigator = () => (
   <Stack.Navigator>
@@ -17,6 +17,6 @@ const AuthNavigator = () => (
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
-)
+);
 
-export default AuthNavigator
+export default AuthNavigator;
